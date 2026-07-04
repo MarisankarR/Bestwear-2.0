d783060a060d2357cd79caa408906ac920b0cdaa
R. Mari <marisankar8888@gmail.com>
2026-07-04 09:05:06 +0530

    Restore database configuration files

---
+++ backend/config/mongodb.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+    mongoose.connection.on('connected', () => {
+        console.log('MONGODB Connected Successful')
+    })
+
+    const dbName = process.env.MONGODB_DB || 'PROJECT'
+    const uri = process.env.MONGODB_URI
+
+    if (!uri) {
+        throw new Error('MONGODB_URI is not defined in environment variables')
+    }
+
+    const connectionUri = uri.includes('?')
+        ? uri.replace(/\/?\?/, `/${dbName}?`)
+        : `${uri}/${dbName}`
+
+    await mongoose.connect(connectionUri)
+}
+
+export default connectDB
